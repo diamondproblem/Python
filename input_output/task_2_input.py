af16a436d219ea264d61af1177df0253242cf8f5
Karolina Rapacz <59149304+diamondproblem@users.noreply.github.com>
2020-12-15 21:58:14 +0100

    Add files via upload

--- input_output/task_2_input.py
+++ input_output/task_2_input.py
@@ -1,2 +1,2 @@
-name_surname_birt_date = input("Podaj swoje imie, nazwisko i date urodzenia: ")
-print(f"Twoje dane to: {name_surname_birt_date}")
+name_surname_birth_date = input("Podaj swoje imie, nazwisko i date urodzenia: ")
+print(f"Twoje dane to: {name_surname_birth_date}")
