a1b201c8fefc62739241f1f289a252afbf6cd6d5
Karolina Rapacz <59149304+diamondproblem@users.noreply.github.com>
2020-12-15 21:48:45 +0100

    Delete task_5_structure.py

--- task_5_structure.py
+++
@@ -1,6 +0,0 @@
-import os
-for root, directories, files in os.walk("."):
-    path = root.split(os.sep)
-    print((len(path) - 1) * '---', os.path.basename(root))
-    for file in files:
-        print(len(path) * '---', file)
