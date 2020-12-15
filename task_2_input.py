4c8ad8c3006169c58e0f36512a774ba38126d1e1
Karolina Rapacz <59149304+diamondproblem@users.noreply.github.com>
2020-12-15 21:48:20 +0100

    Delete task_2_input.py

--- task_2_input.py
+++
@@ -1,6 +0,0 @@
-name = input("Podaj swoje imie: ")
-print("Twoje imie to: {name}".format(name=name))
-surname = input("Podaj swoje nazwisko: ")
-print("Twoje imie to: {surname}".format(surname=surname))
-birthDate = input("Podaj swoja date urodzenia: ")
-print("Twoja data urodzenia to: {birthDate}".format(birthDate=birthDate))
