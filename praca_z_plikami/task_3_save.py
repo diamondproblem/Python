165c4ad0becf0084607d18eb46afd758602de0b4
Karolina Rapacz <59149304+diamondproblem@users.noreply.github.com>
2020-12-15 21:38:42 +0100

    Delete task_3_save.py

--- praca_z_plikami/task_3_save.py
+++
@@ -1,7 +0,0 @@
-fixedKey = "8888"
-inputKey = input("Prosze podac kod: ")
-
-if inputKey == fixedKey:
-    print("Wprowadzony zostal poprawny klucz")
-else:
-    print("Wprowadzony zostal niepoprawny klucz")
